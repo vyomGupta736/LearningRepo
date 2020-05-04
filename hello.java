4d5284ee51a771239a46fe6c2aaf81d6ba0616e6
unknown <vyomgupta456@gmail.com>
2020-05-05 01:43:34 +0530

    dev changed

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ public class hello
 	public static void main(String args[])
 	{
 		System.out.println("hello");
+
+		System.out.println(" i am in dev branch of hello");
 	}
 } 
--- loops.java
+++ loops.java
@@ -3,5 +3,9 @@ public class loops
   public static void main(String[] args) {
   	
   	System.out.print("loops");
+
+  	System.ou.println("done");
+
+  	System.out.println("i am in dev branch of for loops");
   }
 }
